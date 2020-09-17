9bfe46152174a51b358a18e73a6768c8720a5ade
Tamas <jonastamas2@gmail.com>
2020-09-17 11:36:38 +0100

    init: initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Gallery from './components/Gallery';
+import Form from './components/Form';
+
+const App = () => {
+  return (
+    <>
+      <Gallery />
+      <Form />
+    </>
+  );
+};
+
+export default App;
